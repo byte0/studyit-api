6afde03f6c3ceaf36f7fdd3712cbe87b9e0de8c9
code1031 <wangzijie1031@qq.com>
2017-02-23 14:09:22 +0800

    db

--- application/database.php
+++ application/database.php
@@ -17,9 +17,9 @@ return [
     // 数据库名
     'database'       => 'bxg',
     // 用户名
-    'username'       => 'root',
+    'username'       => 'tom',
     // 密码
-    'password'       => 'feiyue',
+    'password'       => '123',
     // 端口
     'hostport'       => '3306',
     // 连接dsn
