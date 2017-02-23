f7c17ba6102a5c034c9d5e7f9256e69717842ff1
code1031 <wangzijie1031@qq.com>
2017-02-23 12:42:58 +0800

    bug fixed

--- public/index.php
+++ public/index.php
@@ -16,13 +16,13 @@ define('APP_PATH', __DIR__ . '/../application/');
 // 绑定模块
 define('BIND_MODULE','api');
 
-$requestUri = $_SERVER['REQUEST_URI'];
+// $requestUri = $_SERVER['REQUEST_URI'];
 
-$dbName = empty(explode('/', $requestUri)[1]) ?
-    'bxg' : !strstr(explode('/', $requestUri)[1], 'v') ?
-    'bxg' : explode('/', $requestUri)[1];
+// $dbName = empty(explode('/', $requestUri)[1]) ?
+//     'bxg' : !strstr(explode('/', $requestUri)[1], 'v') ?
+//     'bxg' : explode('/', $requestUri)[1];
 
-define('DB_NAME', $dbName);
+// define('DB_NAME', $dbName);
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
